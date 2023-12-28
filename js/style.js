2caf1ba0165998abd558c8e3f7b5db320555e772
rk00717 <58892434+rk00717@users.noreply.github.com>
2023-12-29 01:52:21 +0530

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,3 +1,5 @@
+const ERROR_DISPLAY = "ERR"
+
 function set_theme(value){
     document.body.classList.toggle("dark-theme", value);
     document.body.classList.toggle("light-theme", !value);
@@ -21,37 +23,37 @@ function init_theme(){
 var button_container = document.getElementById("button_container");
 
 function init_buttons(){
-    add_button("AC", function(){ clear_values()}, "first-row-set", "btn-invert");
-    add_button("CE", function(){ remove_value()}, "first-row-set", "btn-invert");
+    add_button("AC", "CLEAR", "first-row-set", "btn-invert");
+    add_button("CE", "REMOVE", "first-row-set", "btn-invert");
 
-    add_button("%", function(){ add_value("%") }, "first-row-set");
-    add_button("/", function(){ add_value("/") }, "first-row-set");
+    add_button("%", "ADD", "first-row-set");
+    add_button("/", "ADD", "first-row-set");
     
-    add_button("7", function(){ add_value("7") });
-    add_button("8", function(){ add_value("8") });
-    add_button("9", function(){ add_value("9") });
+    add_button("7");
+    add_button("8");
+    add_button("9");
 
-    add_button("*", function(){ add_value("*") });
+    add_button("*");
     
-    add_button("4", function(){ add_value("4") });
-    add_button("5", function(){ add_value("5") });
-    add_button("6", function(){ add_value("6") });
+    add_button("4");
+    add_button("5");
+    add_button("6");
 
-    add_button("-", function(){ add_value("-") });
+    add_button("-");
     
-    add_button("1", function(){ add_value("1") });
-    add_button("2", function(){ add_value("2") });
-    add_button("3", function(){ add_value("3") });
+    add_button("1");
+    add_button("2");
+    add_button("3");
 
-    add_button("+", function(){ add_value("+") });
+    add_button("+");
     
-    add_button("0", function(){ add_value("0") });
+    add_button("0");
 
-    add_button(".", function(){ add_value(".") });
-    add_button("=", calculate, "w-2", "btn-invert");
+    add_button(".");
+    add_button("=", "CALCULATE", "w-2", "btn-invert");
 }
 
-function add_button(innerElement, clickAction = null, ...classNames){
+function add_button(innerElement, clickAction = "ADD", ...classNames){
     var button = document.createElement("button");
     button.innerHTML = innerElement;
     button.classList.add("calc-btn");
@@ -60,8 +62,23 @@ function add_button(innerElement, clickAction = null, ...classNames){
         button.classList.add(name);
     });
 
-    if(clickAction != null)
-        button.addEventListener("click", clickAction);
+    switch(clickAction){
+        case "ADD":
+            button.addEventListener("click", function(){add_value(innerElement)});
+            break;
+        case "CLEAR":
+            button.addEventListener("click", function(){clear_values()});
+            break;
+        case "REMOVE":
+            button.addEventListener("click", function(){remove_value()});
+            break;
+        case "CALCULATE":
+            button.addEventListener("click", function(){calculate()});
+            break;
+        default:
+            console.log(typeof(innerElement) + " " + innerElement);
+            break;
+    }
     
     button_container.appendChild(button);
 }
@@ -70,22 +87,22 @@ var result_display = document.getElementById("result_display");
 var didCalculate = false;
 
 function add_value(value){
-    if(result_display.value == "0"){
+    if(result_display.value == "0" || result_display.value == ERROR_DISPLAY){
         result_display.value = value;
         return;
     }
-
+    
     result_display.value += value;
 }
 
 function remove_value(){
-    if(didCalculate){
-        didCalculate = false;
+    if(result_display.value == ERROR_DISPLAY){
         result_display.value = "0";
         return;
     }
 
     result_display.value = result_display.value.slice(0, -1);
+    
     if(result_display.value == ""){
         result_display.value = "0";
     }
@@ -105,6 +122,6 @@ function calculate(){
 
         result_display.value = eval(result_display.value);
     }catch(e){
-        result_display.value = "ERR";
+        result_display.value = ERROR_DISPLAY;
     }
 }
