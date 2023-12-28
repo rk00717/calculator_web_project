da53a24517742fcb5f510ab272408b1504d993c3
rk00717 <58892434+rk00717@users.noreply.github.com>
2023-12-29 01:35:48 +0530

    Implemented Backend

--- js/style.js
+++ js/style.js
@@ -1,59 +1,57 @@
+function set_theme(value){
+    document.body.classList.toggle("dark-theme", value);
+    document.body.classList.toggle("light-theme", !value);
+}
+
 function init_theme(){
-    const themeToggle = document.getElementById("themeToggle");
-    const body = document.body;
-    
-    const isDarkTheme = localStorage.getItem("theme") == "dark";
+    const themeToggle = document.getElementById("themeToggle");    
     
     themeToggle.addEventListener('change', function() {
-        body.classList.toggle("dark-theme", this.checked);
-        body.classList.toggle("light-theme", !this.checked);
+        set_theme(this.checked);
         
         let theme = (this.checked)? "dark" : "light";
         localStorage.setItem("theme", theme);
     });
     
-
+    const isDarkTheme = localStorage.getItem("theme") == "dark";
     themeToggle.checked = isDarkTheme;
-    body.classList.toggle("dark-theme", isDarkTheme);
-    body.classList.toggle("light-theme", !isDarkTheme);
+    set_theme(isDarkTheme);
 }
 
-var button_container;
+var button_container = document.getElementById("button_container");
 
 function init_buttons(){
-    button_container = document.getElementById("button_container");
+    add_button("AC", function(){ clear_values()}, "first-row-set", "btn-invert");
+    add_button("CE", function(){ remove_value()}, "first-row-set", "btn-invert");
 
-    add_button("AC", "first-row-set", "btn-invert")
-    add_button("CE", "first-row-set", "btn-invert")
-
-    add_button("%", "first-row-set")
-    add_button("/", "first-row-set")
+    add_button("%", function(){ add_value("%") }, "first-row-set");
+    add_button("/", function(){ add_value("/") }, "first-row-set");
     
-    add_button("7")
-    add_button("8")
-    add_button("9")
+    add_button("7", function(){ add_value("7") });
+    add_button("8", function(){ add_value("8") });
+    add_button("9", function(){ add_value("9") });
 
-    add_button("*")
+    add_button("*", function(){ add_value("*") });
     
-    add_button("4")
-    add_button("5")
-    add_button("6")
+    add_button("4", function(){ add_value("4") });
+    add_button("5", function(){ add_value("5") });
+    add_button("6", function(){ add_value("6") });
 
-    add_button("-")
+    add_button("-", function(){ add_value("-") });
     
-    add_button("1")
-    add_button("2")
-    add_button("3")
+    add_button("1", function(){ add_value("1") });
+    add_button("2", function(){ add_value("2") });
+    add_button("3", function(){ add_value("3") });
 
-    add_button("+")
+    add_button("+", function(){ add_value("+") });
     
-    add_button("0")
+    add_button("0", function(){ add_value("0") });
 
-    add_button(".")
-    add_button("=", "w-2", "btn-invert")
+    add_button(".", function(){ add_value(".") });
+    add_button("=", calculate, "w-2", "btn-invert");
 }
 
-function add_button(innerElement, ...classNames){
+function add_button(innerElement, clickAction = null, ...classNames){
     var button = document.createElement("button");
     button.innerHTML = innerElement;
     button.classList.add("calc-btn");
@@ -61,6 +59,52 @@ function add_button(innerElement, ...classNames){
     classNames.forEach(function (name) {
         button.classList.add(name);
     });
+
+    if(clickAction != null)
+        button.addEventListener("click", clickAction);
     
     button_container.appendChild(button);
+}
+
+var result_display = document.getElementById("result_display");
+var didCalculate = false;
+
+function add_value(value){
+    if(result_display.value == "0"){
+        result_display.value = value;
+        return;
+    }
+
+    result_display.value += value;
+}
+
+function remove_value(){
+    if(didCalculate){
+        didCalculate = false;
+        result_display.value = "0";
+        return;
+    }
+
+    result_display.value = result_display.value.slice(0, -1);
+    if(result_display.value == ""){
+        result_display.value = "0";
+    }
+}
+
+function clear_values(){
+    result_display.value = "0";
+}
+
+function calculate(){
+    didCalculate = true;
+
+    try{
+        if(result_display.value == ""){
+            result_display.value = "0";
+        }
+
+        result_display.value = eval(result_display.value);
+    }catch(e){
+        result_display.value = "ERR";
+    }
 }
